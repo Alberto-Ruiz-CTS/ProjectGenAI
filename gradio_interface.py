0849a3a949ef0964f5fcbec4f9ccd42ccee2d783
egaleote <eduardo.galeote@cognizant.com>
2024-07-16 12:26:42 +0000

    added vector embeddings

--- gradio_interface.py
+++ gradio_interface.py
@@ -2,12 +2,70 @@ import gradio as gr
 import const
 import json
 import requests
+from pypdf import PdfReader
+from helper_utils import word_wrap
+from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+import chromadb
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
+
 
 # Authorization header for the API
 headers = {"Authorization": f"Bearer {const.API_KEYedu}"}
 
 # URL for the API
 url = "https://api.edenai.run/v2/text/chat"
+
+# Read pdf BOE
+reader = PdfReader("/teamspace/studios/this_studio/ProjectGenAI/BOE-A-1978-31229-consolidado.pdf")
+pdf_texts = [p.extract_text().strip() for p in reader.pages]
+
+# Filter the empty strings
+pdf_texts = [text for text in pdf_texts if text]
+
+# We divide our pdf in chunks in that order, so no chunk is larger than 1000
+character_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ". ", " ", ""],
+    chunk_size=1000,
+    chunk_overlap=0
+)
+character_split_texts = character_splitter.split_text('\n\n'.join(pdf_texts))
+
+# Redivide our chunks according to token count so our chunks capture all the meaning
+token_splitter = SentenceTransformersTokenTextSplitter(chunk_overlap=0, tokens_per_chunk=256)
+
+token_split_texts = []
+for text in character_split_texts:
+    token_split_texts += token_splitter.split_text(text)
+
+print(f"\nTotal chunks: {len(token_split_texts)}")
+
+# Uses the BERT architecture but slight different architecture to embed whole chunks, not just single words
+embedding_function = SentenceTransformerEmbeddingFunction()
+
+# We embed all our chunks and pass them into chroma to build the framework
+chroma_client = chromadb.Client()
+chroma_collection = chroma_client.create_collection("boe", embedding_function=embedding_function)
+
+ids = [str(i) for i in range(len(token_split_texts))]
+
+chroma_collection.add(ids=ids, documents=token_split_texts)
+
+# Querying RAG, embedding our query and Retrieving similar chunks
+
+query = "¿Puede alguien ser miembro de varias Cámaras?"
+
+results = chroma_collection.query(query_texts=[query], n_results=5)
+retrieved_documents = results['documents'][0]
+
+for document in retrieved_documents:
+    print(word_wrap(document))
+    print('\n')
+
+
+
+
+
+
 
 # Initial conversation history
 conversation_history = []
@@ -78,4 +136,4 @@ with gr.Blocks() as demo:
     clear_button.click(fn=clear_history, inputs=None, outputs=conversation_output)
 
 # Launch the interface
-demo.launch(share=True)
+#demo.launch(share=True)
